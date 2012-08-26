35f9a66f0e1a91501eedffd9d4bdf9e2a83c3a79
phaneendra <phaneendragodspeed@gmail.com>
2012-08-26 10:20:52 +0800

    Update SublimeQuickFileCreator.py
    Added support for automatically creating directories from the file input box.
    This way it is possible to select any directory and type the relative path to create the directory structure and file.

--- SublimeQuickFileCreator.py
+++ SublimeQuickFileCreator.py
@@ -80,12 +80,23 @@ class QuickCreateFileCreatorBase(sublime_plugin.WindowCommand):
             dir = self.selected_dir
 
         full_path = os.path.join(self.root, dir, file_name)
+
         if os.path.lexists(full_path):
             sublime.error_message('File already exists:\n%s' % full_path)
             return
         else:
             self.create_and_open_file(full_path)
 
+    def create(self, filename):
+        base, filename = os.path.split(filename)
+        self.create_folder(base)
+
+    def create_folder(self, base):
+        if not os.path.exists(base):
+            parent = os.path.split(base)[0]
+            if not os.path.exists(parent):
+                self.create_folder(parent)
+            os.mkdir(base)
 
 class QuickCreateFileCommand(QuickCreateFileCreatorBase):
     INPUT_PANEL_CAPTION = 'File name:'
@@ -94,7 +105,8 @@ class QuickCreateFileCommand(QuickCreateFileCreatorBase):
         self.doCommand()
 
     def create_and_open_file(self, path):
-        open(path, 'w')
+        if not os.path.exists(path):
+            self.create(path)
         self.window.open_file(path)
 
 
